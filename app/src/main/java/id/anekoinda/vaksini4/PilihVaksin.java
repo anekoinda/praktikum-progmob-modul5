cbd7b53d599d57bf7971e4809f56ced616d01c45
Shilta Inda <shiltainda@gmail.com>
2021-12-20 13:12:25 +0800

    fix icon daftar form

---
+++ app/src/main/java/id/anekoinda/vaksini4/PilihVaksin.java
@@ -0,0 +1,14 @@
+package id.anekoinda.vaksini4;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PilihVaksin extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pilih_vaksin);
+    }
+}
