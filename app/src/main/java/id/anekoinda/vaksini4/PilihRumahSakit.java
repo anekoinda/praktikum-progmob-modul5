13afb9d9d30e3578aeec786f5bfa28df9af8d107
Shilta Inda <shiltainda@gmail.com>
2021-12-20 13:52:52 +0800

    pilih rs layout

---
+++ app/src/main/java/id/anekoinda/vaksini4/PilihRumahSakit.java
@@ -0,0 +1,14 @@
+package id.anekoinda.vaksini4;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PilihRumahSakit extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pilih_rumah_sakit);
+    }
+}
