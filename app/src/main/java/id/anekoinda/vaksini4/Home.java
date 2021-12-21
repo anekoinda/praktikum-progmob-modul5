1dabf66c107f243f918964bd3c15a3d8300f40a5
dikapra12 <dprasetiyoutomo12@gmail.com>
2021-12-21 20:29:29 +0800

    menyambung ke database

--- app/src/main/java/id/anekoinda/vaksini4/Home.java
+++ app/src/main/java/id/anekoinda/vaksini4/Home.java
@@ -3,18 +3,100 @@ package id.anekoinda.vaksini4;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.SeekBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class Home extends AppCompatActivity {
-    TextView vaksin;
-    @Override
+    static int kondisi_kesehatan=0;
+    Button button_daftar;
+    EditText nik, nama, telepon;
+    CheckBox check_tidak, check_flu, check_hamil;
+    RadioGroup radioGroup;
+    RadioButton jenis_kelamin;
+    SeekBar seekbar_kondisi;
+    TextView persentase_kondisi;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        DBHelper db = new DBHelper(this);
 
-        vaksin = findViewById(R.id.vaksin);
+        nik = findViewById(R.id.input_nik);
+        nama = findViewById(R.id.input_nama);
+        telepon = findViewById(R.id.input_telepon);
+        button_daftar = findViewById(R.id.button_daftar);
+        radioGroup = findViewById(R.id.radio_jenkel);
+        check_tidak = findViewById(R.id.check_tidak);
+        check_flu = findViewById(R.id.check_flu);
+        check_hamil = findViewById(R.id.check_hamil);
+        seekbar_kondisi = findViewById(R.id.seekbar_kondisi);
+        persentase_kondisi = findViewById(R.id.persentase);
+        seekbar_kondisi.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                persentase_kondisi.setText(progress + ("%"));
+            }
 
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+            }
 
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+            }
+        });
+
+        button_daftar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int selected_jenkel = radioGroup.getCheckedRadioButtonId();
+                jenis_kelamin = findViewById(selected_jenkel);
+
+                String kondisi_kesehatan = "";
+                if(check_flu.isChecked() && check_hamil.isChecked()){
+                    kondisi_kesehatan += "Flu, Hamil";
+                }
+                else{
+                    if(check_tidak.isChecked()) {
+                        kondisi_kesehatan += "Tidak ada keluhan";
+                    }
+                    if(check_flu.isChecked()) {
+                        kondisi_kesehatan += "Flu";
+                    }
+                    if(check_hamil.isChecked()) {
+                        kondisi_kesehatan += "Hamil";
+                    }
+                }
+                String txt_nik =  nik.getText().toString();
+                String txt_nama =  nama.getText().toString();
+                String txt_telepon =  telepon.getText().toString();
+                String txt_jenkel = jenis_kelamin.getText().toString();
+                String txt_kondisi = kondisi_kesehatan;
+                String txt_persentase = String.valueOf(seekbar_kondisi.getProgress());
+
+                Boolean checkinsert = db.insert(txt_nik, txt_nama, txt_telepon, txt_jenkel, txt_kondisi, txt_persentase);
+                if(checkinsert == true){
+                    Toast.makeText(Home.this, "Pendaftaran berhasil", Toast.LENGTH_SHORT).show();
+                }else{
+                    Toast.makeText(Home.this, "Pendaftaran gagal", Toast.LENGTH_SHORT).show();
+                }
+
+                nik.setText(null);
+                nama.setText(null);
+                telepon.setText(null);
+                radioGroup.clearCheck();
+                check_flu.setChecked(false);
+                check_hamil.setChecked(false);
+                check_tidak.setChecked(false);
+                seekbar_kondisi.setProgress(0);
+            }
+        });
     }
 }
