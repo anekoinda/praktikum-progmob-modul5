578f2e291f585c4ba371b7eb385df71b4000705f
NooNNata <tudemahardika27@gmail.com>
2021-12-21 22:34:09 +0800

    test

--- app/src/main/java/id/anekoinda/vaksini4/daftar.java
+++ app/src/main/java/id/anekoinda/vaksini4/daftar.java
@@ -14,59 +14,11 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class RegisterActivity extends AppCompatActivity {
-
-    EditText InputNama, InputNik, InputTelepon, InputPassword;
-    Button ButtonLogin;
-    DBHelper dbHelper;
+public class daftar extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_daftar);
-
-        dbHelper = new DBHelper(this);
-
-        InputNama = (EditText)findViewById(R.id.inputNama);
-        InputNik = (EditText)findViewById(R.id.inputNik);
-        InputTelepon = (EditText)findViewById(R.id.inputTelepon);
-        InputPassword = (EditText)findViewById(R.id.inputPassword);
-        ButtonLogin = (Button)findViewById(R.id.buttonLogin);
-
-
-        ButtonLogin.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String nama = InputNama.getText().toString().trim();
-                String nik = InputNik.getText().toString().trim();
-                String telepon = InputTelepon.getText().toString().trim();
-                String password = InputPassword.getText().toString().trim();
-                ContentValues values = new ContentValues();
-
-
-                 if (password.equals("") || nama.equals("")){
-                    Toast.makeText(RegisterActivity.this, "Username or Password cannot be empty", Toast.LENGTH_SHORT).show();
-                }else {
-                    values.put(DBHelper.row_nik, Niklogin);
-                    values.put(DBHelper.row_password, Password);
-                    dbHelper.insertData(values);
-
-                    Toast.makeText(RegisterActivity.this, "Register successful", Toast.LENGTH_SHORT).show();
-                    finish();
-                }
-            }
-        });
-
-
-    }
-
-    public static Spanned fromHtml(String html){
-        Spanned result;
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N){
-            result = Html.fromHtml(html, Html.FROM_HTML_MODE_LEGACY);
-        }else {
-            result = Html.fromHtml(html);
-        }
-        return result;
     }
 }
--- app/src/main/java/id/anekoinda/vaksini4/login.java
+++ app/src/main/java/id/anekoinda/vaksini4/login.java
@@ -13,64 +13,7 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class LoginActivity extends AppCompatActivity {
-
-    EditText InputNik, InputPassword;
-    Button ButtonLogin;
-    TextView buttonDaftar;
-    DBHelper dbHelper;
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
-
-        InputNik = (EditText)findViewById(R.id.inputNik);
-        InputPassword = (EditText)findViewById(R.id.inputPassword);
-        ButtonLogin = (Button)findViewById(R.id.buttonLogin);
-
-        dbHelper = new DBHelper(this);
-
-        TextView buttonDaftar = (TextView)findViewById(R.id.buttonDaftar);
-
-        buttonDaftar.setText(fromHtml("I don't have account yet. " +
-                "</font><font color='#3b5998'>create one</font>"));
-        buttonDaftar.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(LoginActivity.this, daftar.class));
-            }
-        });
-
-        ButtonLogin.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String username = InputNik.getText().toString().trim();
-                String password = InputPassword.getText().toString().trim();
-
-                Boolean res = dbHelper.checkUser(nik,password);
-                if(res == true){
-                    Toast.makeText(LoginActivity.this, "Login Successful", Toast.LENGTH_SHORT).show();
-                    startActivity(new Intent(LoginActivity.this, MainActivity.class));
-                }else {
-                    Toast.makeText(LoginActivity.this, "Login Failed", Toast.LENGTH_SHORT).show();
-                }
-            }
-        });
-    }
-
-    public static Spanned fromHtml(String html){
-        Spanned result;
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N){
-            result = Html.fromHtml(html, Html.FROM_HTML_MODE_LEGACY);
-        }else {
-            result = Html.fromHtml(html);
-        }
-        return result;
-    }
-}
-
-//public class login extends AppCompatActivity {
+public class login extends AppCompatActivity {
 
     Button buttonLogin;
     TextView buttonDaftar;
